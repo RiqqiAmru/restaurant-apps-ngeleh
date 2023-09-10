1dbe442b0b64be3c7a7dbb9c29b6df67a7066f0e
RiqqiAmru <Riqqia00@gmail.com>
2023-09-10 10:05:40 +0700

    folder docs gh-pages

--- webpack.common.js
+++ webpack.common.js
@@ -36,7 +36,7 @@ module.exports = {
       patterns: [
         {
           from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/'),
+          to: path.resolve(__dirname, 'docs/'),
         },
       ],
     }),
